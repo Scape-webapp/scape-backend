a2d19914ae025b611d50dd30525f8a990d573e46
poonam-kumawat <mailpoonam2002@gmail.com>
2024-01-16 23:38:18 +0530

    some changes

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -9,8 +9,9 @@ import {
 } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageBody } from './dto/message.dto';
-import { MessageHistoryBody } from './dto/message-history.dto';
+import { MessageChatBody, MessageHistoryBody } from './dto/message-history.dto';
 // import { MessageHistoryBody } from './dto/message-history.dto';
+import mongoose, { ObjectId } from 'mongoose';
 
 @Controller('message')
 export class MessageController {
@@ -23,28 +24,52 @@ export class MessageController {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
-  @Post('/history')
-  async find(@Body() messageHistoryBody: MessageHistoryBody) {
-    try {
-      const filters = {
-        $or: [
-          {
-            sender: messageHistoryBody.id,
-          },
-          {
-            receiver: messageHistoryBody.id,
-          },
-        ],
-      };
-      return this.messageService.find(filters);
-    } catch (error) {
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
-    }
-  }
+  // @Post('/history')
+  // async find(@Body() messageHistoryBody: MessageHistoryBody) {
+  //   try {
+  //     const filters = {
+  //       $or: [
+  //         {
+  //           sender: messageHistoryBody.id,
+  //         },
+  //         {
+  //           receiver: messageHistoryBody.id,
+  //         },
+  //       ],
+  //     };
+  //     return this.messageService.find(filters);
+  //   } catch (error) {
+  //     throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+  //   }
+  // }
   @Get('/chat-list/:id')
   async ChatList(@Param() id: string) {
     try {
       return this.messageService.fetchLastMsgList(id);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+  @Post('/chat')
+  async find(@Body() messageChatBody: MessageChatBody) {
+    try {
+      const receiverId = new mongoose.Types.ObjectId(
+        messageChatBody.receiver.toString(),
+      );
+      const senderId = new mongoose.Types.ObjectId(
+        messageChatBody.sender.toString(),
+      );
+      const filters = {
+        // receiver: messageChatBody.receiver,
+        // sender: messageChatBody.receiver,
+        sender: {
+          $or: [{ receiver: receiverId }, { sender: senderId }],
+        },
+        receiver: {
+          $or: [{ receiver: receiverId }, { sender: senderId }],
+        },
+      };
+      return this.messageService.find(filters);
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
