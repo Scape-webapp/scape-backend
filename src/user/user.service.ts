c686c809865ccb308b779e300333016f66edc33e
poonam-kumawat <mailpoonam2002@gmail.com>
2024-01-22 00:32:56 +0530

    user search api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -27,6 +27,13 @@ export class UserService {
     ).lean();
   }
 
+  async findByUsername(username: string) {
+    // return this.UserModel.findOne({ username }).lean();
+    return this.UserModel.findOne({
+      user_name: username,
+    }).lean();
+  }
+
   findOne(id: string) {
     return this.UserModel.findById(new mongoose.Types.ObjectId(id)).lean();
   }
